b8007c644b96058fa05b1a888697171a1603867b
JUSTIN C WILLIAMS <justinwdev@gmail.com>
2022-03-29 16:05:45 -0400

    Add routes that don't need a controller.

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('about', function () {
+    return view('about');
+});
+
+Route::get('portrait', function () {
+    return view('portait');
+});
+
+Route::get('wedding', function () {
+    return view('wedding');
 });
 
 Route::get('/dashboard', function () {
