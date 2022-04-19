a5c1064f9a3bc23ba35955be4fea5d8e32b65318
Justin Williams <justinwdev@gmail.com>
2022-04-19 04:53:27 -0400

    çAdd tailwind cdn.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,7 @@
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <script src="https://cdn.tailwindcss.com"></script>
 
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
