5989d71b5b744775f32ef90e5d8d15289d9e6633
Justin Williams <justinwdev@gmail.com>
2022-04-23 15:08:01 -0400

    Removing tailwind cdn

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -12,11 +12,10 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Styles -->
-        <link rel="stylesheet" href="{{ secure_asset('css/app.css') }}">
-        <script src="https://cdn.tailwindcss.com"></script>
+        <link rel="stylesheet" href="/css/app.css">
 
         <!-- Scripts -->
-        <script src="{{ secure_asset('js/app.js') }}" defer></script>
+        <script src='/js/app.js' defer></script>
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
