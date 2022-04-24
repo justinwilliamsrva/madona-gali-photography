060387e168ba6893feefa19642d175e9d60d4909
Justin Williams <justinwdev@gmail.com>
2022-04-24 14:53:31 -0400

    Fix Tailwind image issue

--- resources/views/components/banner-image.blade.php
+++ resources/views/components/banner-image.blade.php
@@ -1,4 +1,4 @@
-<div class="w-full bg-cover bg-center bg-[url('{{ $url }}')]" style="height:32rem;">
+<div class="w-full bg-cover bg-center {{ $backgroundImage }}" style="height:32rem;">
     <div class="flex items-center justify-center h-full w-full bg-gray-900 bg-opacity-50">
         <div class="text-center">
             <h1 class="text-white text-2xl font-semibold uppercase md:text-3xl">{{ $headingTitle }}</h1>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,6 @@
 <x-guest-layout>
     <x-slot name="header">
-        <x-banner-image url='/images/home-banner.jpeg' headingTitle='EVERY PHOTO TELLS A STORY' />
+        <x-banner-image backgroundImage="bg-[url('/images/home-banner.jpeg')]" headingTitle='EVERY PHOTO TELLS A STORY' />
     </x-slot>
 
     <x-reviews.review-create :reviews='$reviews'/>
