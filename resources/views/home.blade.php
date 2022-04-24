8cbc2440aebc1ef824c16e9e5b43ab859afce340
Justin Williams <justinwdev@gmail.com>
2022-04-24 07:30:41 -0400

    Remove Login card

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,14 +3,5 @@
         <x-banner-image url='/images/home-banner.jpeg' headingTitle='EVERY PHOTO TELLS A STORY' />
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
-                </div>
-            </div>
-        </div>
-    </div>
     <x-reviews.review-create :reviews='$reviews'/>
 </x-guest-layout>
