ecc3e74c3ec7a81ab309b623d018375b30a687eb
Justin Williams <justinwdev@gmail.com>
2022-04-25 05:40:35 -0400

    Add work section.

---
+++ resources/views/components/home-product-cards.blade.php
@@ -0,0 +1,8 @@
+<a href="{{$link}}" class="group">
+    <div class="w-full aspect-w-1 aspect-h-1 rounded-lg overflow-hidden sm:aspect-w-2 sm:aspect-h-3">
+        <img src="{{ $image }}" alt="Paper card sitting upright in walnut card holder on desk." class="w-full h-full object-center object-cover group-hover:opacity-75">
+    </div>
+    <div class="mt-4 flex items-center justify-center text-base font-medium text-black">
+        <h3>{{ $title }}</h3>
+    </div>
+</a>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,22 @@
         <x-banner-image backgroundImage="bg-[url('/images/home-banner.jpeg')]" headingTitle='EVERY PHOTO TELLS A STORY' />
     </x-slot>
 
-    <x-reviews.review-create :reviews='$reviews'/>
+    <div class="bg-white">
+        <div class="text-center">
+            <h1>LOCATED IN</h1>
+            <h2>Richmond ,VA</h2>
+            <h2>Available Worldwide</h2>ß
+        </div>
+        <div class="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+            <div class="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:gap-x-8">
+            <x-home-product-cards link="#" image="/images/home-wedding.jpeg" title="Wedding" />
+            <x-home-product-cards link="#" image="/images/home-couples.jpeg" title="Couples" />
+            <x-home-product-cards link="#" image="/images/home-seniors.jpeg" title="Seniors" />
+            <x-home-product-cards link="#" image="/images/home-family.jpeg" title="Family" />
+            <x-home-product-cards link="#" image="/images/home-maternity.jpeg" title="Maternity" />
+            <x-home-product-cards link="#" image="/images/home-real-estate.jpeg" title="Real Estate" />
+            </div>
+        </div>
+    </div>
+    <x-reviews.review-create :reviews='$reviews' />
 </x-guest-layout>
