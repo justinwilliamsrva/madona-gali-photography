cf513e7ca0c8c42e6e90a6ebe0fc43c91e262278
Justin Williams <justinwdev@gmail.com>
2022-05-02 17:25:38 -0400

    About page.

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,24 +1,38 @@
 <x-guest-layout>
     <x-slot name="header">
-        <x-banner-image backgroundImage="bg-[url('/images/home-banner.jpeg')]" headingTitle='EVERY PHOTO TELLS A STORY' />
+        <x-banner-image backgroundImage="bg-[url('/images/home-banner.jpeg')]" headingTitle='ABOUT ME' />
     </x-slot>
 
-    <div class="bg-[#EAF3F6]">
-        <div class="text-center py-8">
-            <h1 class="text-2xl">LOCATED IN</h1>
-            <h2 class="text-2xl">Richmond ,VA</h2>
-            <h2 class="text-xl italic">Available Worldwide</h2>
-        </div>
-        <hr />
-        <div class="max-w-2xl mx-auto py-8 px-4 sm:py-12 sm:px-6 lg:max-w-7xl lg:px-8">
-        <h1 class="text-black text-center text-2xl font-semibold uppercase md:text-3xl mb-6">Services</h1>
-            <div class="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:gap-x-8">
-                <x-home-product-cards link="wedding" image="/images/home-wedding.jpeg" title="Wedding" />
-                <x-home-product-cards link="couples" image="/images/home-couples.jpeg" title="Couples" />
-                <x-home-product-cards link="seniors" image="/images/home-seniors.jpeg" title="Seniors" />
-                <x-home-product-cards link="family" image="/images/home-family.jpeg" title="Family" />
-                <x-home-product-cards link="maternity" image="/images/home-maternity.jpeg" title="Maternity" />
-                <x-home-product-cards link="real-estate" image="/images/home-real-estate.jpeg" title="Real Estate" />
+    <div class="bg-[#EAF3F6] overflow-hidden">
+        <div class="relative max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
+            <div class="hidden lg:block bg-gray-50 absolute top-0 bottom-0 left-3/4 w-screen"></div>
+            <div class="mx-auto text-base max-w-prose lg:grid lg:grid-cols-2 lg:gap-8 lg:max-w-none">
+                <div>
+                    <h2 class="text-base text-indigo-600 font-semibold tracking-wide uppercase">Madonna Gali</h2>
+                    <h3 class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">Hey there, Glad you made it here!</h3>
+                </div>
+            </div>
+            <div class="mt-8 lg:grid lg:grid-cols-2 lg:gap-8">
+                <div class="relative lg:row-start-1 lg:col-start-2">
+                    <div class="relative text-base mx-auto max-w-prose lg:max-w-none">
+                        <figure>
+                            <div class="aspect-w-12 aspect-h-7 lg:aspect-none">
+                                <img class="rounded-lg shadow-lg object-cover object-center" src="/images/about-main.jpeg" alt="Whitney leaning against a railing on a downtown street" width="1184" height="1376">
+                            </div>
+                        </figure>
+                    </div>
+                </div>
+                <div class="mt-8 lg:mt-0">
+                    <div class="text-base max-w-prose mx-auto lg:max-w-none">
+                        <p class="text-lg mb-5 text-gray-500">I'm Madona, and I am passionate about using photography to bring others together in making joyful, lasting memories and images! I'm originally from Egypt, but have called Richmond, VA home for the past 20 years. My perfect day starts with a cup of coffee; includes catching my breath running after my two sons; trying out a new recipe and nailing it makes the day even better; because we just LOVE food and ends with spending time with my favorite people—my husband, family, and friends!</p>
+                        <p class="text-lg mb-5 text-gray-500">I see beauty in the world all around me, and especially in the clients I have the privilege to work with. It is my great joy to help others see and discover that beauty in their own lives and families.  As your photographer, I seek to draw out the delight in your connections with those you love, so that we can create photos of those sweet moments for you to enjoy the rest of your life!</p>
+                        <p class="text-lg mb-5 text-gray-500">II am a documentary portrait and wedding photographer primarily serving the Richmond, Virginia area; however, I am always up for a collaboration or a fun destination venue. Specializing in weddings, engagements, senior, maternity, family portraits and real estate photography, I am delighted at the opportunity to work with you!</p>
+                        <p class="text-lg mb-5 text-gray-500">Thank you for taking the time to get to know me, and I am thrilled to meet and work with you. Let's make some beautiful memories and preserve them forever in photos!</p>
+                        <p class="text-lg mb-5 text-gray-500">Joyfully, </p>
+                        <p class="text-xl ml-12 text-gray-500">Madona</p>
+
+                    </div>
+                </div>
             </div>
         </div>
     </div>
