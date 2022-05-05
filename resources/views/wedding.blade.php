d5e62a2c672288075de9115a567ff344cb59b080
Justin Williams <justinwdev@gmail.com>
2022-05-05 06:12:21 -0400

    Add wedding.

---
+++ resources/views/wedding.blade.php
@@ -0,0 +1,49 @@
+<x-guest-layout>
+    <x-slot name="header">
+        <x-banner-text title='WEDDINGS' text='fine art photography for the classic romantic, laid back bride who is oh so in love with her fiancé, loves planning all of the little details, and believes that marrying her best friend is the most important aspect of the day' />
+    </x-slot>
+
+    <div id="carouselExampleControls" class="carousel slide relative" data-bs-ride="carousel">
+        <div class=" carousel-inner relative w-full overflow-hidden">
+            <div class="carousel-item active relative float-left w-full">
+                <img src="/images/wedding/wedding-1.jpeg" class="block mx-auto h-[32rem]" alt="Wild Landscape" />
+            </div>
+            <div class="carousel-item relative float-left w-full">
+                <img src="/images/wedding/wedding-2.jpeg" class="block mx-auto h-[32rem]" alt="" />
+            </div>
+            <div class="carousel-item relative float-left w-full">
+                <img src="/images/wedding/wedding-3.jpeg" class="block mx-auto h-[32rem]" alt="" />
+            </div>
+            <div class="carousel-item relative float-left w-full">
+                <img src="/images/wedding/wedding-4.jpeg" class="block mx-auto h-[32rem]" alt="" />
+            </div>
+            <div class="carousel-item relative float-left w-full">
+                <img src="/images/wedding/wedding-5.jpeg" class="block mx-auto h-[32rem]" alt="" />
+            </div>
+            <div class="carousel-item relative float-left w-full">
+                <img src="/images/wedding/wedding-6.jpeg" class="block mx-auto h-[32rem]" alt="" />
+            </div>
+            <div class="carousel-item relative float-left w-full">
+                <img src="/images/wedding/wedding-7.jpeg" class="block mx-auto h-[32rem]" alt="" />
+            </div>
+            <div class="carousel-item relative float-left w-full">
+                <img src="/images/wedding/wedding-8.jpeg" class="block mx-auto h-[32rem]" alt="" />
+            </div>
+            <div class="carousel-item relative float-left w-full">
+                <img src="/images/wedding/wedding-9.jpeg" class="block mx-auto h-[32rem]" alt="" />
+            </div>
+            <div class="carousel-item relative float-left w-full">
+                <img src="/images/wedding/wedding-91.jpeg" class="block mx-auto h-[32rem]" alt="" />
+            </div>
+        </div>
+        <button class="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon inline-block bg-no-repeat" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+            <span class="carousel-control-next-icon inline-block bg-no-repeat" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+
+</x-guest-layout>
