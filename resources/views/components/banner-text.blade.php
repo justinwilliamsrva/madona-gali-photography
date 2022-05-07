4443e739d6548a234fb6f2f9560b742c1ee145f5
Justin Williams <justinwdev@gmail.com>
2022-05-07 04:42:37 -0400

    Add details to wedding and portrait page

--- resources/views/components/banner-text.blade.php
+++ resources/views/components/banner-text.blade.php
@@ -1,4 +1,4 @@
-<div class="w-full bg-cover bg-center bg-[#EAF3F6]" style="height:32rem;">
+<div class="w-full bg-cover bg-center bg-[#EAF3F6]border-t-2" style="height:32rem;">
             <div class="flex items-start justify-center h-full w-full">
                 <div class="max-w-7xl pt-32 px-4 space-y-8 text-center">
                     <h1 class="text-black text-4xl font-semibold uppercase md:text-5xl">{{$title}}</h1>
--- resources/views/wedding.blade.php
+++ resources/views/wedding.blade.php
@@ -46,4 +46,209 @@
         </button>
     </div>
 
+
+    <div class="min-h-[32rem] bg-[#EAF3F6]">
+        <div class="relative p-4 mx-auto max-w-5xl sm:px-6 lg:px-8" style="cursor: auto;">
+            <div class="max-w-lg mx-auto my-5 overflow-hidden shadow-lg lg:max-w-none lg:flex">
+                <div class="flex-1 px-6 py-8 bg-white lg:p-12" style="cursor: auto;">
+                    <h3 class="text-2xl font-extrabold text-gray-900 sm:text-3xl" style="cursor: auto;">Micro Wedding</h3>
+                    <div class="mt-8">
+                        <div class="flex items-center">
+                            <h4 class="flex-shrink-0 pr-4 text-sm font-semibold tracking-wide uppercase bg-white">What's included</h4>
+                            <div class="flex-1 border-t-2 border-gray-200"></div>
+                        </div>
+                        <ul class="mt-8 space-y-5 lg:space-y-0 lg:grid lg:grid-cols-2 lg:gap-x-8 lg:gap-y-5">
+                            <li class="flex items-start lg:col-span-1">
+                                <div class="flex-shrink-0">
+                                    <svg class="w-5 h-5 text-green-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
+                                    </svg>
+                                </div>
+                                <p class="ml-3 text-sm text-gray-700">4 Hours of Coverage</p>
+                            </li>
+                            <li class="flex items-start lg:col-span-1">
+                                <div class="flex-shrink-0">
+                                    <svg class="w-5 h-5 text-green-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
+                                    </svg>
+                                </div>
+                                <p class="ml-3 text-sm text-gray-700">Professional Editing</p>
+                            </li>
+                            <li class="flex items-start lg:col-span-1">
+                                <div class="flex-shrink-0">
+                                    <svg class="w-5 h-5 text-green-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
+                                    </svg>
+                                </div>
+                                <p class="ml-3 text-sm text-gray-700">Digital Images</p>
+                            </li>
+                            <li class="flex items-start lg:col-span-1">
+                                <div class="flex-shrink-0">
+                                    <svg class="w-5 h-5 text-green-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
+                                    </svg>
+                                </div>
+                                <p class="ml-3 text-sm text-gray-700">Printing Rights</p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="px-6 py-8 text-center bg-gray-50 lg:flex-shrink-0 lg:flex lg:flex-col lg:justify-center lg:p-12" style="cursor: auto;">
+                    <p class="mt-4 text-gray-500" style="text-decoration-style:solid">starting at</p>
+                    <div class="flex items-center justify-center mt-4 text-5xl font-extrabold text-gray-900">
+                        <span>$1,500</span>
+                    </div>
+                    <div class="mt-6">
+                        <div class="rounded-md shadow">
+                            <a href="{{route('contact')}}" class="flex items-center justify-center w-full px-5 py-3 text-base font-medium text-white bg-gray-800 border border-transparent rounded-md hover:bg-gray-900" target="_blank">Contact Me</a>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <div class="max-w-lg mx-auto my-5 overflow-hidden shadow-lg lg:max-w-none lg:flex">
+                <div class="flex-1 px-6 py-8 bg-white lg:p-12" style="cursor: auto;">
+                    <h3 class="text-2xl font-extrabold text-gray-900 sm:text-3xl" style="cursor: auto;">Romantic Wedding</h3>
+                    <div class="mt-8">
+                        <div class="flex items-center">
+                            <h4 class="flex-shrink-0 pr-4 text-sm font-semibold tracking-wide uppercase bg-white">What's included</h4>
+                            <div class="flex-1 border-t-2 border-gray-200"></div>
+                        </div>
+                        <ul class="mt-8 space-y-5 lg:space-y-0 lg:grid lg:grid-cols-2 lg:gap-x-8 lg:gap-y-5">
+
+                            <li class="flex items-start lg:col-span-1">
+                                <div class="flex-shrink-0">
+                                    <svg class="w-5 h-5 text-green-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
+                                    </svg>
+                                </div>
+                                <p class="ml-3 text-sm text-gray-700">6 Hours of Coverage</p>
+                            </li>
+                            <li class="flex items-start lg:col-span-1">
+                                <div class="flex-shrink-0">
+                                    <svg class="w-5 h-5 text-green-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
+                                    </svg>
+                                </div>
+                                <p class="ml-3 text-sm text-gray-700">Complementary Engagement Session</p>
+                            </li>
+                            <li class="flex items-start lg:col-span-1">
+                                <div class="flex-shrink-0">
+                                    <svg class="w-5 h-5 text-green-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
+                                    </svg>
+                                </div>
+                                <p class="ml-3 text-sm text-gray-700">2nd Shooter</p>
+                            </li>
+                            <li class="flex items-start lg:col-span-1">
+                                <div class="flex-shrink-0">
+                                    <svg class="w-5 h-5 text-green-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
+                                    </svg>
+                                </div>
+                                <p class="ml-3 text-sm text-gray-700">Digital Images</p>
+                            </li>
+                            <li class="flex items-start lg:col-span-1">
+                                <div class="flex-shrink-0">
+                                    <svg class="w-5 h-5 text-green-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
+                                    </svg>
+                                </div>
+                                <p class="ml-3 text-sm text-gray-700">Printing Rights</p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="px-6 py-8 text-center bg-gray-50 lg:flex-shrink-0 lg:flex lg:flex-col lg:justify-center lg:p-12" style="cursor: auto;">
+                    <p class="mt-4 text-gray-500" style="text-decoration-style:solid">starting at</p>
+                    <div class="flex items-center justify-center mt-4 text-5xl font-extrabold text-gray-900">
+                        <span>$2,250</span>
+                    </div>
+                    <div class="mt-6">
+                        <div class="rounded-md shadow">
+                            <a href="{{route('contact')}}" class="flex items-center justify-center w-full px-5 py-3 text-base font-medium text-white bg-gray-800 border border-transparent rounded-md hover:bg-gray-900" target="_blank">Contact Me</a>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="max-w-lg mx-auto my-5 overflow-hidden shadow-lg lg:max-w-none lg:flex">
+                <div class="flex-1 px-6 py-8 bg-white lg:p-12" style="cursor: auto;">
+                    <h3 class="text-2xl font-extrabold text-gray-900 sm:text-3xl" style="cursor: auto;">Enduring Wedding</h3>
+                    <div class="mt-8">
+                        <div class="flex items-center">
+                            <h4 class="flex-shrink-0 pr-4 text-sm font-semibold tracking-wide uppercase bg-white">What's included</h4>
+                            <div class="flex-1 border-t-2 border-gray-200"></div>
+                        </div>
+                        <ul class="mt-8 space-y-5 lg:space-y-0 lg:grid lg:grid-cols-2 lg:gap-x-8 lg:gap-y-5">
+
+                            <li class="flex items-start lg:col-span-1">
+                                <div class="flex-shrink-0">
+                                    <svg class="w-5 h-5 text-green-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
+                                    </svg>
+                                </div>
+                                <p class="ml-3 text-sm text-gray-700">8 Hours of Coverage</p>
+                            </li>
+                            <li class="flex items-start lg:col-span-1">
+                                <div class="flex-shrink-0">
+                                    <svg class="w-5 h-5 text-green-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
+                                    </svg>
+                                </div>
+                                <p class="ml-3 text-sm text-gray-700">Complementary Bridal Session</p>
+                            </li>
+                            <li class="flex items-start lg:col-span-1">
+                                <div class="flex-shrink-0">
+                                    <svg class="w-5 h-5 text-green-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
+                                    </svg>
+                                </div>
+                                <p class="ml-3 text-sm text-gray-700">Complementary Engagement Session</p>
+                            </li>
+                            <li class="flex items-start lg:col-span-1">
+                                <div class="flex-shrink-0">
+                                    <svg class="w-5 h-5 text-green-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
+                                    </svg>
+                                </div>
+                                <p class="ml-3 text-sm text-gray-700">2nd Shooter</p>
+                            </li>
+                            <li class="flex items-start lg:col-span-1">
+                                <div class="flex-shrink-0">
+                                    <svg class="w-5 h-5 text-green-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
+                                    </svg>
+                                </div>
+                                <p class="ml-3 text-sm text-gray-700">Online Gallery</p>
+                            </li>
+                            <li class="flex items-start lg:col-span-1">
+                                <div class="flex-shrink-0">
+                                    <svg class="w-5 h-5 text-green-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
+                                    </svg>
+                                </div>
+                                <p class="ml-3 text-sm text-gray-700">Printing Rights to All Images From Each Session</p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="px-6 py-8 text-center bg-gray-50 lg:flex-shrink-0 lg:flex lg:flex-col lg:justify-center lg:p-12" style="cursor: auto;">
+                    <p class="mt-4 text-gray-500" style="text-decoration-style:solid">starting at</p>
+                    <div class="flex items-center justify-center mt-4 text-5xl font-extrabold text-gray-900">
+                        <span>$2,900</span>
+                    </div>
+                    <div class="mt-6">
+                        <div class="rounded-md shadow">
+                            <a href="{{route('contact')}}" class="flex items-center justify-center w-full px-5 py-3 text-base font-medium text-white bg-gray-800 border border-transparent rounded-md hover:bg-gray-900" target="_blank">Contact Me</a>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+
 </x-guest-layout>
