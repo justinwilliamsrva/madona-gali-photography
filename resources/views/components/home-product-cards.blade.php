ba08490ce7c434b38a970dcd095b7cdc065f8de3
Justin Williams <justinwdev@gmail.com>
2022-06-04 22:06:19 -0400

    Fix services images on home page.

--- resources/views/components/home-product-cards.blade.php
+++ resources/views/components/home-product-cards.blade.php
@@ -1,8 +1,8 @@
-<a class="group">
-    <div class="w-3/4 mx-auto md:w-full items-center aspect-w-1 aspect-h-1 rounded-lg overflow-hidden sm:aspect-w-2 sm:aspect-h-3">
-        <img src="{{ $image }}" alt="Paper card sitting upright in walnut card holder on desk." class="w-full h-full object-center object-cover group-hover:opacity-75">
-    </div>
-    <div class="mt-4 flex items-center justify-center text-base font-medium text-black">
-        <h3>{{ $title }}</h3>
-    </div>
+<a href="#" class="group h-[32rem] sm:h-96 {{ $image == 'hide' ? 'hidden xl:invisible xl:block' : 'visible'}}">
+  <div class="w-full h-full bg-gray-200 rounded-lg overflow-hidden">
+    <img src="{{ $image }}" alt="" class=" w-full h-full object-center object-cover group-hover:opacity-75">
+  </div>
+  <div class="mt-4 flex items-center justify-center text-base font-medium text-black">
+    <h3>{{ $title }}</h3>
+  </div>
 </a>
