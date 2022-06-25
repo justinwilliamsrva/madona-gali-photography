1b0ed5a2eab2dca2ee5bc60b5b067a2964af2cd2
Justin Williams <justinwdev@gmail.com>
2022-06-25 14:13:33 -0400

    fix size of home images.

--- resources/views/components/home-product-cards.blade.php
+++ resources/views/components/home-product-cards.blade.php
@@ -1,6 +1,6 @@
-<a href="{{ route($link)}} " class="group h-[32rem] sm:h-96">
+<a href="{{ route($link)}} " class="group h-80">
   <div class="w-full h-full bg-gray-200 rounded-lg overflow-hidden">
-    <img src="{{ $image }}" alt="" class=" w-full h-full object-center object-cover group-hover:opacity-75">
+    <img src="{{ $image }}" alt="" class="{{($title == 'Family' || $title == 'Maternity') ? 'object-top' : 'object-center'}} w-full h-full object-cover group-hover:opacity-75">
   </div>
   <div class="mt-4 flex items-center justify-center text-base font-medium text-black">
     <h3>{{ $title }}</h3>
