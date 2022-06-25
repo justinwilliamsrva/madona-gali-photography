1797ed58b5aee4077be425135e5cf3f88a660130
Justin Williams <justinwdev@gmail.com>
2022-06-25 14:34:36 -0400

    Fix home banner links.

--- resources/views/components/home-product-cards.blade.php
+++ resources/views/components/home-product-cards.blade.php
@@ -1,8 +1,10 @@
-<a href="{{ route($link)}} " class="group h-80">
-  <div class="w-full h-full bg-gray-200 rounded-lg overflow-hidden">
-    <img src="{{ $image }}" alt="" class="{{($title == 'Family' || $title == 'Maternity') ? 'object-top' : 'object-center'}} w-full h-full object-cover group-hover:opacity-75">
-  </div>
+<div class="h-80">
+  <a href="{{ route($link)}} " class="group">
+    <div class="w-full h-full bg-gray-200 rounded-lg overflow-hidden">
+      <img src="{{ $image }}" alt="" class="{{($title == 'Family' || $title == 'Maternity') ? 'object-top' : 'object-center'}} w-full h-full object-cover group-hover:opacity-75">
+    </div>
+  </a>
   <div class="mt-4 flex items-center justify-center text-base font-medium text-black">
     <h3>{{ $title }}</h3>
   </div>
-</a>
+</div>
