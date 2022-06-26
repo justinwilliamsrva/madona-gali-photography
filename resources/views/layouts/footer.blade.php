2bb36e73c2e2705e986cb912e9154a7221d481b4
Justin Williams <justinwdev@gmail.com>
2022-06-26 15:31:57 -0400

    Fix footer.

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -29,13 +29,6 @@
       </div>
     </nav>
     <div class="mt-8 flex justify-center space-x-6">
-      <a href="https://www.facebook.com/MadonaGaliPhotography" class="text-gray-700 hover:text-gray-900">
-        <span class="sr-only">Facebook</span>
-        <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-          <path fill-rule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clip-rule="evenodd" />
-        </svg>
-      </a>
-
       <a href="https://www.instagram.com/madonagali/?hl=en" class="text-gray-700 hover:text-gray-900">
         <span class="sr-only">Instagram</span>
         <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
@@ -50,7 +43,8 @@
         </svg>
       </a>
     </div>
-    <p class="mt-8 text-center text-base text-gray-700">&copy; {{ date('Y') }} Copyright Modana Gali Photography</p>
+    <p class="mt-8 text-center text-base text-gray-700">&copy; {{ date('Y') }} Copyright<br class="sm:hidden"/> Modana Gali Photography</p>
+    <p class="text-center text-4xl text-gray-700">-</p>
     <p class="text-center text-base text-gray-700">Designed By <a href="https://www.justinwdev.com" target="_blank">Justin Williams</a></p>
   </div>
 </footer>
