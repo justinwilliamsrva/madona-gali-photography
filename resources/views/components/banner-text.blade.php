85db3b71497ecb61e17bfe4956fa0c472ecfdc76
Justin Williams <justinwdev@gmail.com>
2022-06-30 06:07:29 -0400

    Fix padding of banner text.

--- resources/views/components/banner-text.blade.php
+++ resources/views/components/banner-text.blade.php
@@ -1,7 +1,7 @@
 <div class="w-full bg-cover bg-center bg-[#EAF3F6]">
     <div class="max-w-5xl mx-auto border-t-2 h-[25rem] md:h-[32rem]">
         <div class="flex items-start justify-center h-full w-full">
-            <div class="max-w-7xl pt-20 md:pt-32 px-4 space-y-8 text-center">
+            <div class="max-w-7xl pt-10 sm:pt-15 md:pt-32 px-4 space-y-8 text-center">
                 <h1 class="text-black text-4xl font-semibold uppercase md:text-5xl font-normal tracking-widest">{{$title}}</h1>
                 <p class="text-l sm:text-2xl text-center">{{$text}}</p>
             </div>
